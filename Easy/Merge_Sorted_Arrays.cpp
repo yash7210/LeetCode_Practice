1242fa634b45bd0f1f1c6272c2fe87ef22d5e595
yash7210 <85038009+yash7210@users.noreply.github.com>
2025-01-10 01:22:12 -0500

    Update Merge_Sorted_Arrays.cpp

--- Easy/Merge_Sorted_Arrays.cpp
+++ Easy/Merge_Sorted_Arrays.cpp
@@ -1,6 +1,8 @@
 // My attempted code 
 class Solution {
 public:
+
+// moves each element in the array one index to the right 
    void moveOver(vector<int>& arr, int temp, int index, int arr_size){
         int temp2;
         for(int j = index; j < arr_size; j++){
@@ -10,12 +12,17 @@ public:
         }
 
     }
+
+   
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        if (n == 0){
+       // if nums2 is an empty array nothing needs to be done
+       if (n == 0){
             return;
         }
+       
         int temp;
+       
         // first combine both arrays
         int j = 0;
         for(int i = m; i < m + n; i ++){
@@ -24,9 +31,12 @@ public:
         }
 
         for(int i = 0; i < m; i++){
+           // compare to check if elements need to swap
             if(nums1[m] < nums1[i]){
+                // insert element at correct index
                 temp = nums1[i];
                 nums1[i] = nums1[m];
+                // move over the rest of the elements
                 moveOver(nums1, temp, i + 1, m + n);
             }
         }
